75f1b1221f894ae154b16febb85af06439bdb3fb
ceciisalas <cecizs1@hotmail.com>
2022-08-02 18:42:22 +0000

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
-"""Module for BaseModel class"""
-from datetime import datetime
+"""Base class for the AirBnB clone"""
 import uuid
-import models
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """BaseModel Class"""
+    """
+    Defines all attributes and method for other classes
+    """
+
     def __init__(self, *args, **kwargs):
-        """Initialization of BaseModel"""
-        if len(kwargs) != 0:
-            self.__dict__ = kwargs
-            self.created_at = datetime.strptime(self.created_at,  %Y-%m-%dT%H:%M:%S.%f)
-            self.updated_at = datetime.strptime(self.updated_at,  %Y-%m-%dT%H:%M:%S.%f)
-            
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-
-            def __str__(self):
-                """Return a string representation of an object"""
-                return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
-
-            def save(self):
-                """Updates the date for updated_at"""
+        """Initializes objects"""
+        if kwargs:
+            for key, value in kwargs.items()
+            if key != '__class__':
+                setattr(self, key, value)
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime-strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
                 self.updated_at = datetime.now()
-                storage.save()
+                storage.new(self)
 
-                def to_dict(self):
-                    """Returns dictionary containing
-                    all keys/values of __dict__
-                    """
-                    newdict = self.__dict__.copy()
-                    newdict['__class__'] = self.__class__.__name__
-                    newdict['created_at'] = self.created_at.isoformat()
-                    newdict['updated_at'] = self.updated_at.isoformat()
+                def __str__(self):
+                    """Print name, id, dict"""
+                    return '[{}] ({}) {}'.format(self.__class__.name, self.id, self.__dict__)
 
-                    return newdict
+                def save(self):
+                    """Updates the updated_at attribute with the current datetime"""
+                    self.updated_at = datetime.now()
+                    storage.save()
+
+                    def to_dict(self):
+                        """Returns dictionary"""
+                        new_dict = self.__dict__.copy()
+                        new_dict['__class__'] = self.__class__.name__
+                        new_dict['created_at'] = self.created_at.isoformat()
+                        new_dict['updated_at'] = self.updated_at.isoformat()
+
+                        return new_dict
