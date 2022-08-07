0d929f5e10b4aece498c2c19cc5452c77c764e68
ceciisalas <cecizs1@hotmail.com>
2022-08-07 17:21:44 +0000

    updated

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""City class that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class City:
+    """Attributes of the city class"""
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""State class that inherit from BaseModel"""
+from models.base_model import BaseModel
+
+
+class State:
+    """Attribute for name of the state"""
+    name = ""
